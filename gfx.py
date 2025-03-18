37426c9c6ce571d2011f3f16a40d23d0077864d7
PRMenon <pratheek.menon@gmail.com>
2025-03-18 06:14:21 +0000

    added cell

--- gfx.py
+++ gfx.py
@@ -1,5 +1,4 @@
 from tkinter import Canvas
-import logging
 
 class Point():
     def __init__(self, x: int, y: int):
@@ -13,11 +12,6 @@ class Line():
     def __init__(self, start: Point, end: Point):
         self.start: Point = start
         self.end: Point = end
-        # self.x1 = start.x
-        # self.y1 = start.y
-        # self.x2 = end.x
-        # self.y2 = end.y
-        logging.info(self)
 
     def draw(self, canvas: Canvas, fill_color: str):
         canvas.create_line(
@@ -28,4 +22,49 @@ class Line():
         return f"Line between {self.start} and {self.end}"
     
 class Cell():
+    def __init__(
+            self, window, top_left: Point, bottom_right: Point, 
+            has_left_wall: bool = True, has_right_wall: bool = True,
+            has_top_wall: bool = True, has_bottom_wall: bool = True,
+            ):
+        
+        self._win = window
+        self.top_left: Point = top_left
+        self.bottom_right: Point = bottom_right
+        self.top_right: Point = Point(self.bottom_right.x, self.top_left.y)
+        self.bottom_left: Point = Point(self.top_left.x, self.bottom_right.y)
+
+        self.has_left_wall = has_left_wall
+        self.has_right_wall= has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+
+        self.mid_x = (self.top_left.x + self.top_right.x) // 2
+        self.mid_y = (self.top_left.y + self.bottom_left.y) // 2
+
+    def draw(self):
+        if self.has_top_wall:
+            self._win.draw_line(Line(self.top_left, self.top_right),
+                                "black")
+
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(self.bottom_left, self.bottom_right),
+                                "black")
+
+        if self.has_left_wall:
+            self._win.draw_line(Line(self.top_left, self.bottom_left),
+                                "black")
+
+        if self.has_right_wall:
+            self._win.draw_line(Line(self.top_right, self.bottom_right),
+                                "black")
+
+    def draw_move(self, to_cell, undo: bool = False):
+        color = "red" if undo else "gray"
+        self._win.draw_line(
+            Line(Point(self.mid_x, self.mid_y),
+                 Point(to_cell.mid_x, to_cell.mid_y)),
+                 color)
+
+class Maze():
     ...
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from window import Window
-from gfx import Line, Point
+from gfx import Line, Point, Cell
 import logging
 
 def main():
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting the window")
     win = Window(800, 600)
-    line1 = Line(Point(100, 200), Point(200, 200))
-    line2 = Line(Point(200, 300), Point(300, 400))
-    win.draw_line(line1, "red")
-    win.draw_line(line2, "black")
+    cell1 = Cell(win, Point(200,300), Point(300, 400), has_right_wall=False)
+    cell2 = Cell(win, Point(300,300), Point(400, 400), has_left_wall=False)
+    cell1.draw()
+    cell2.draw()
+    cell1.draw_move(cell2)
     win.wait_for_close()
 
 if __name__ == "__main__":
