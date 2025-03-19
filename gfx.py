0873452c193144a095e8a381d5a79ad3971422bf
PRMenon <pratheek.menon@gmail.com>
2025-03-19 06:33:19 +0000

    added maze basics and refactor

--- gfx.py
+++ gfx.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from time import sleep
 
 class Window:
     def __init__(self, width: int, height: int):
@@ -12,7 +13,7 @@ class Window:
         
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
     
-    def draw_line(self, line: "Line", fill_color: str) -> None:
+    def draw_line(self, line: "Line", fill_color: str = "black") -> None:
         line.draw(self.__canvas, fill_color)
     
     def redraw(self) -> None:
@@ -52,49 +53,87 @@ class Line():
     
 
 class Cell():
-    def __init__(
-            self, window: "Window", top_left: "Point", bottom_right: "Point", 
-            has_left_wall: bool = True, has_right_wall: bool = True,
-            has_top_wall: bool = True, has_bottom_wall: bool = True,
-            ):
+    def __init__(self, window: "Window"):
+        self.has_left_wall: bool = True
+        self.has_right_wall: bool = True
+        self.has_top_wall: bool = True
+        self.has_bottom_wall: bool = True
+        self._x1: int = 0
+        self._x2: int = 0
+        self._y1: int = 0
+        self._y2: int = 0
+        self._win = window
+
+    def draw(self, x1: int, y1: int, x2: int, y2: int):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         
-        self._win: Window = window
-        self.top_left: Point = top_left
-        self.bottom_right: Point = bottom_right
-        self.top_right: Point = Point(self.bottom_right.x, self.top_left.y)
-        self.bottom_left: Point = Point(self.top_left.x, self.bottom_right.y)
-
-        self.has_left_wall: bool = has_left_wall
-        self.has_right_wall: bool= has_right_wall
-        self.has_top_wall: bool = has_top_wall
-        self.has_bottom_wall: bool = has_bottom_wall
-
-        self.mid_x: int = (self.top_left.x + self.top_right.x) // 2
-        self.mid_y: int = (self.top_left.y + self.bottom_left.y) // 2
-
-    def draw(self) -> None:
-        if self.has_top_wall:
-            self._win.draw_line(Line(self.top_left, self.top_right),
-                                "black")
-
-        if self.has_bottom_wall:
-            self._win.draw_line(Line(self.bottom_left, self.bottom_right),
-                                "black")
-
         if self.has_left_wall:
-            self._win.draw_line(Line(self.top_left, self.bottom_left),
-                                "black")
-
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        
         if self.has_right_wall:
-            self._win.draw_line(Line(self.top_right, self.bottom_right),
-                                "black")
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+    
+    def _get_midpoint(self) -> "Point":
+        return Point(
+            abs(self._x1 - self._x2) // 2,
+            abs(self._y1 - self._y2) // 2)
 
-    def draw_move(self, to_cell: "Cell", undo: bool = False) -> None:
-        color: str = "red" if undo else "gray"
-        self._win.draw_line(
-            Line(Point(self.mid_x, self.mid_y),
-                 Point(to_cell.mid_x, to_cell.mid_y)),
-                 color)
+    def draw_move(self, to_cell: "Cell", undo=False):   
+        fill_color = "gray" if undo else "red"
+        line: Line = Line(self._get_midpoint(), to_cell._get_midpoint())
+        self._win.draw_line(line, fill_color)
 
 class Maze():
-    ...
+    def __init__(self, x1: int, y1: int,
+                 num_rows: int, num_cols: int,
+                 cell_size_x: int, cell_size_y: int,
+                 win: "Window"):
+        self._x1: int = x1
+        self._y1: int = y1 
+        self._num_rows: int = num_rows
+        self._num_cols: int = num_cols
+        self._cell_size_x: int = cell_size_x
+        self._cell_size_y: int = cell_size_y
+        self._win: "Window" = win
+
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self) -> None:
+        dummy_cell: "Cell" = Cell(self._win)
+        dummy_col = [dummy_cell] * self._num_rows
+        self._cells = [dummy_col] * self._num_cols
+        
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cells(i, j)
+
+    def _draw_cells(self, i: int, j: int) -> None:
+        x1: int = self._x1 + i * self._cell_size_x
+        y1: int = self._y1 + j * self._cell_size_y
+
+        x2: int = x1 + self._cell_size_x
+        y2: int = y1 + self._cell_size_y
+
+        current_cell: "Cell" = self._cells[i][j]
+        current_cell.draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        sleep(0.05)
+
+        
